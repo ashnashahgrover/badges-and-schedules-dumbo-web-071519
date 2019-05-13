867553fae36a21b5304254c23d318efdd5cc576e
Ashna Shah-Grover <as19@williams.edu>
2019-05-13 16:02:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,3 +16,6 @@ def assign_rooms(speakers)
   for i in 0..6 do  
     assign = "Hello, #{speakers[i]}! You'll be assigned to room #{[i+1]}!"
     assignments.push(assign)
+  end 
+  return assignments
+end 
